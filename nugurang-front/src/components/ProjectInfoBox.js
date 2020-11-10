55745eded7b866e6c9091b67b4644f2af9693f95
Hyeonmin <chohm00790083@gmail.com>
2020-11-11 00:44:07 +0900

    connect users in project

--- nugurang-front/src/components/ProjectInfoBox.js
+++ nugurang-front/src/components/ProjectInfoBox.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import Avatar from '@material-ui/core/Avatar';
 import AvatarGroup from '@material-ui/lab/AvatarGroup';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 
 export default function ProjectInfoBox({ project }) {
+  const router = useRouter();
   return (
-    <Grid container spacing={2} alignItems="center" justify="center">
+    <Grid container spacing={2} alignItems="center" justify="space-between">
       <Grid item container spacing={2} alignItems="center" justify="flex-start">
         {
           project.getUsers
@@ -26,6 +29,11 @@ export default function ProjectInfoBox({ project }) {
           <Typography variant="h4">
             {project.name}
           </Typography>
+        </Grid>
+        <Grid item alignContent='flex-end'>
+          <Box justifyContent='flex-end'>
+            <Button onClick={() => router.push({pathname: "/peer-review", query: {project: router.query.id}})}>Terminate Project</Button>
+          </Box>
         </Grid>
       </Grid>
     </Grid>
--- nugurang-front/src/pages/peer-review/index.js
+++ nugurang-front/src/pages/peer-review/index.js
@@ -1,14 +1,16 @@
 import { gql, useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
 import React from 'react';
+
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import BaseMultiSelect from '../../components/BaseMultiSelect';
 
 import Layout from '../../components/Layout';
 
-import BaseMultiSelect from '../../components/BaseMultiSelect';
+
 import SectionBox from '../../components/SectionBox';
 import SectionTitleBar from '../../components/SectionTitleBar';
 
@@ -23,24 +25,31 @@ const GET_POSITIONS = gql`
       name
     }
   }
-`
+`;
 
 const GET_TEAMMATES = gql`
+  query getProject($id: ID!) {
+    getProject(id: $id) {
+      id
+      getUsers(page: 0, pageSize: 100) {
+        id
+        name
+        image {
+          id
+          address
+        }
+      }
+    }
+  }
+}
+}
+`;
+
+export const GET_CURRENT_USER = gql`
   query {
     currentUser {
       id
-      getTeams(page: 0, pageSize: 100) {
-        id
-        name
-        getUsers(page: 0, pageSize: 100) {
-          id
-          name
-          image {
-            id
-            address
-          }
-        }
-      }
+      name
     }
   }
 `;
@@ -116,7 +125,8 @@ function PeerReviewIndex() {
   const router = useRouter();
   const responses = [
     useQuery(GET_POSITIONS),
-    useQuery(GET_TEAMMATES),
+    useQuery(GET_TEAMMATES, {variables: {id: router.query.project}}),
+    useQuery(GET_CURRENT_USER)
   ]
   const errorResponse = responses.find((response) => response.error)
 
@@ -127,34 +137,38 @@ function PeerReviewIndex() {
     return <Loading />;
 
   const Positions = responses[0].data.positions
-  const users = responses[1].data.currentUser.getTeams ? responses[1].data.currentUser.getTeams[0].getUsers : null;
+  const users = responses[1].data.getProject ? responses[1].data.getProject.getUsers : null;
+  const curUser = responses[2].data.currentUser;
 
   return (
     <Layout>
       <SectionTitleBar title="Peer review" backButton />
       {users.flat().map((item) => (
-        <SectionBox key={item.id} titleBar={<SectionTitleBar title={item.name} avatar={item.image.address} circleIcon="true" />}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Typography variant="body1">Did very well of...</Typography>
-              <BaseMultiSelect
-                items={Positions}
-                label="Position"
-                placeholder="Select position"
-              />
+        item.id !== curUser.id
+        ? (
+          <SectionBox key={item.id} titleBar={<SectionTitleBar title={item.name} avatar={item.image.address} circleIcon="true" />}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography variant="body1">Did very well of...</Typography>
+                <BaseMultiSelect
+                  items={Positions}
+                  label="Position"
+                  placeholder="Select position"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">Needs to go the extra mile to...</Typography>
+                <BaseMultiSelect
+                  items={Positions}
+                  label="Position"
+                  placeholder="Select position"
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <Typography variant="body1">Needs to go the extra mile to...</Typography>
-              <BaseMultiSelect
-                items={Positions}
-                label="Position"
-                placeholder="Select position"
-              />
-            </Grid>
-          </Grid>
-        </SectionBox>
+          </SectionBox>
         )
-      )}
+        : null
+      ))}
       <Box align="center">
         <Button onClick={() => router.push('/peer-review/thank-you')}>Submit</Button>
       </Box>
